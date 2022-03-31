5885f18b7beacde8288ed7b5edb6881e01709775
Boudjada Yasser <by.root96@gmail.com>
2022-03-31 17:40:12 +0100

    Add files via upload

--- examples.py
+++ examples.py
@@ -7,4 +7,3 @@ print(asciii.asciitext("fonts/ANSI_Shadow.txt","#asciitext","#ff0000"))
 # Example:2
 font_url="https://raw.githubusercontent.com/yasserbdj96/asciitext/main/fonts/Calvin_S.txt"
 print(asciii.asciitext(font_url,"#asciitext","#ff0000"))
-
