758f72e1f94117f61348e58e5756a609a1f8abc6
Boudjada Yasser <by.root96@gmail.com>
2022-05-01 18:46:24 +0100

    Add files via upload

--- examples.py
+++ examples.py
@@ -16,11 +16,13 @@ try:
     else:
         #this is error for pass try.
         print(os.environ['X'])
-except:
+except Exception as e:
     # Example:1
     print(asciii.asciitext("fonts/ANSI_Shadow.txt","#asciitext","#ff0000"))
-
 
     # Example:2
     font_url="https://raw.githubusercontent.com/yasserbdj96/asciitext/main/fonts/Calvin_S.txt"
     print(asciii.asciitext(font_url,"#asciitext","#ff0000"))
+
+finally:
+    pass
