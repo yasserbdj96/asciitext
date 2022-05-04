dbe2675cf7a60ab381f7a703e2621f795d1c3bdd
boudjada yasser <yasser.bdj96@gmail.com>
2022-05-04 03:43:57 +0100

    test

--- examples.py
+++ examples.py
@@ -10,19 +10,19 @@ try:
         color_bg = os.environ['COLOR_BG'] if "COLOR_BG" in os.environ else "#000000"
     
         if color_bg=="#000000":
-            print(asciii.asciitext(font,text,color))
+            print(asciii.asciitext(font,text,color).encode("utf-8"))
         else:
-            print(asciii.asciitext(font,text,color,color_bg))
+            print(asciii.asciitext(font,text,color,color_bg).encode("utf-8"))
     else:
         #this is error for pass try.
         print(os.environ['X'])
 except Exception as e:
     # Example:1
-    print(asciii.asciitext("fonts/ANSI_Shadow.txt","#asciitext","#ff0000"))
+    print(asciii.asciitext("fonts/ANSI_Shadow.txt","#asciitext","#ff0000").encode("utf-8"))
 
     # Example:2
     font_url="https://raw.githubusercontent.com/yasserbdj96/asciitext/main/fonts/Calvin_S.txt"
-    print(asciii.asciitext(font_url,"#asciitext","#ff0000"))
+    print(asciii.asciitext(font_url,"#asciitext","#ff0000").encode("utf-8"))
 
 finally:
     pass
